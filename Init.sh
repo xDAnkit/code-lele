0d45e5cbacd35d8f21d8dd1ffde5cd5ebbd27b3b
Yash <yashshukla336@gmail.com>
2023-12-22 18:07:25 +0530

    added some new Node files

---
+++ Init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Navigating to Backend"
+cd server
+
+echo "Installing NPM Moduless"
+npm install
+
+
+echo "Navigating to Frontend"
+cd ../client
+
+echo "Installing NPM Moduless"
+npm install
+
+echo "Back to root folder & fone"
